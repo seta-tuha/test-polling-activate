6f206f68b8a33261e8f3b40a7518790c8d1fb199
seta-tuha <tu.ha@setacinq.vn>
2019-06-21 14:27:27 +0700

    Sample for polling data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import VeritoneActivate, { API_EVENTS } from 'veritone-activate';
+
+const config = {
+  authToken:
+    "288497:4e797c92d71d4efaab7a14ccd1a34d7b73ac132246224e8b8540754ebc1093d2",
+  scheduledJobId: "55849",
+  streamEngineId: "9e611ad7-2d3b-48f6-a51b-0a1ba40feab4",
+  videoInputId: "videoInputId",
+  cognition: {
+    transcription: {
+      language: "en-US" // Parameters are optional
+    },
+    faceDetection: {
+      libraryId: "ab4b6d2-dff4-410f-ab53-5031635a3fda"
+    }
+  },
+  on: {}
+};
+
+const additionalEngines = [{
+  'engineId': '0981e79f-de99-489e-a2c4-366c4b466321',
+  'payload': {
+    'idThreshold': 0.6,
+    'stoppingThreshold': 0.6,
+    'maxSpeakerLabels': 8,
+    'hideUnknown': '0',
+    'libraryId': '5ab4b6d2-dff4-410f-ab53-5031635a3fda'
+  }
+}];
+
+
+const veritoneActivate = new VeritoneActivate(config);
+
 
 function App() {
+  const onInputFile = (event) => {
+    const file = event.target.files[0]
+    const blob = new Blob([file], { type: file.type });
+    veritoneActivate.launchJob(blob, additionalEngines)
+      .then(({ tdoId, jobId }) => {
+        veritoneActivate.startPolling('getEngineResult', tdoId, jobId, '0981e79f-de99-489e-a2c4-366c4b466321');
+      })
+      veritoneActivate.on('getEngineResult', (message) => console.log('from event', message));
+      veritoneActivate.on(API_EVENTS.error, (error) => console.error("error", error));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={onInputFile} />
     </div>
   );
 }
